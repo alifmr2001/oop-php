1255c207dfd459769450b89c494eb7795d8be365
alifmr <alifr4006@gmail.com>
2023-06-21 17:36:27 +0700

    cara mengakses protected property di dalam class

--- enkapsulasi.php
+++ enkapsulasi.php
@@ -13,3 +13,36 @@ class siswa
 $siswa_sd = new siswa();
 $siswa_sd->nama = "Budi";
 echo $siswa_sd->nama;
+
+// protected dan cara aksesnya
+// protected property hanya dapat diakses di dalam kelas itu sendiri dan tidak dapat diakses jika diluar kelas
+echo "<br>";
+
+class mobil
+{
+    //property yang sudah di protected
+    protected $brand  = "bella";
+
+    //akses properti method
+    public function pemilik_mobil()
+    {
+        return $this->brand;
+    }
+
+    protected function hidupkan_mobile()
+    {
+        return "hidupkan mobil bella";
+    }
+
+    public function paksa_hidup()
+    {
+        return $this->hidupkan_mobile();
+    }
+}
+
+//Buat objek baru dari kelas mobil
+$mobil_bella = new mobil();
+
+echo $mobil_bella->pemilik_mobil();
+
+echo "<br>" . $mobil_bella->paksa_hidup();
