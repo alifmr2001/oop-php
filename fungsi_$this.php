61da24fbfabbf2614c0702a0aee07eb9a3cd3e7b
alifmr <alifr4006@gmail.com>
2023-06-23 01:01:43 +0700

    class hanyalah kerangka / blueprint yang hanya diakses oleh objek

--- fungsi_$this.php
+++ fungsi_$this.php
@@ -3,5 +3,12 @@
 class laptop
 {
     public $pemilik;
-    // ini adalah komentar
+
+    public function beli_laptop()
+    {
+        return "beli laptop";
+    }
 }
+
+$laptop_a = new laptop();
+echo $laptop_a->pemilik;
